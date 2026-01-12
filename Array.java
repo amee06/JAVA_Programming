17986074decdfe9c07ffdea715f0b915ea7d8185
amee06 <ameenuddh@gmail.com>
2026-01-12 18:04:37 +0530

    new add

--- Array.java
+++ Array.java
@@ -13,7 +13,7 @@ public class Array {
          cars[1] = "BMW";
          cars[2] = "kiya"; 
          System.out.println(cars[1]);
-         System.out.println(cars[5]);  //error
+         //System.out.println(cars[5]);  //error
 
          //=>.leanght to know the size of array
          int size = cars.length;
@@ -21,7 +21,7 @@ public class Array {
 
          //=> loop trowout array
          for(int i = 0; i <= size; i++){
-            System.out.println("index : " + i + " value : " + cars[i]);
+          //  System.out.println("index : " + i + " value : " + cars[i]);
          }
 
          //=> Use it when you dont know the size 
@@ -34,12 +34,18 @@ public class Array {
     class Student {
         int roll_no;
         String name;
-        float mark;
+        double mark;
 
-        public Student(int r,String n,float m) {
-            r = roll_no;
-            n = name;
-            m = mark;
+        public Student(int r,String n,double m) {
+            roll_no = r;
+            name = n;
+            mark = m;
+        }
+        public void display(){
+            System.out.println("Roll number = " + roll_no);
+            System.out.println("Name = " + name);
+            System.out.println("Mark = " + mark);
+            System.out.println("-------------------");
         }
     }
 
@@ -50,13 +56,17 @@ public class Array {
             Student[] arr;
             
             //allocating memory for 2 objects of student type
-            arr = new Student[2];
+            arr = new Student[3];
 
             //intalizting value
             arr[0] = new Student(135,"jack",85.32);
             arr[1] = new Student(766,"max",74.23);
-            arr[2] = new Student(652,"rose",.91.56);
+            arr[2] = new Student(652,"rose",91.5);
 
+            for(int i = 0; i < arr.length; i++){
+                System.out.println("\nStudent " + (i+1));
+                arr[i].display();
+            }
         }
     }
 
